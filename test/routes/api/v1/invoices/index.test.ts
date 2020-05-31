79cec594c54d9838962e93a42b149daddd25b54f
Spencer <spencer.feng@icloud.com>
2020-05-31 15:11:21 +1000

    test: create the integration test for the route to create a invoice for a company

---
+++ test/routes/api/v1/invoices/index.test.ts
@@ -0,0 +1,12 @@
+import request from 'supertest'
+
+import { app } from '../../../../../src/app'
+
+describe('invoices routes', () => {
+  it('should have a route handler listening to /api/v1/:company/invoices for post requests', async () => {
+    const companyUuid = '047e9001-0001-5d14-4124-b165ce840ad2'
+    const response = await request(app).post(`/api/v1/${companyUuid}/invoices`).send({})
+
+    expect(response.status).not.toEqual(404)
+  })
+})
