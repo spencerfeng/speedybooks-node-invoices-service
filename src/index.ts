845a8a82dda2bfa95ffbbf45f0e772ea2a04cefa
Spencer <spencer.feng@icloud.com>
2020-06-06 20:11:19 +1000

    refactor: change how TypeORM is configured

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { createConnection } from 'typeorm'
 import { app } from './app'
 
 const start = async () => {
-  await createConnection()
+  await createConnection(process.env.NODE_ENV!)
 
   app.listen(3000, () => {
     console.log('Listening on port 3000!')
