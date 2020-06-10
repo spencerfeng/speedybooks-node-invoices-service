04d38f06f439f38f1aab9f0c27c70a1e60bc3ec6
Spencer <spencer.feng@icloud.com>
2020-06-10 22:10:55 +1000

    feat: add an error handler middleware to handle both sync and async errors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import 'express-async-errors'
 import { config } from 'dotenv'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 
+import { errorHandler } from './middlewares/errorHandler'
 import { invoicesRouterV1 } from './routes/api/v1/invoices'
 
 config()
@@ -13,5 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 
 app.use('/api/v1', invoicesRouterV1)
+
+app.use(errorHandler)
 
 export { app }
