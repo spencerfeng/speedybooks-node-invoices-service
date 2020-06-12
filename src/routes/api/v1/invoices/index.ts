f353bbcbb79a107e87ad127a6725ef4109fc2055
Spencer Feng <spencer.feng@icloud.com>
2020-06-12 22:27:22 +1000

    feat: handle errors properly in the route to create an invoice

--- src/routes/api/v1/invoices/index.ts
+++ src/routes/api/v1/invoices/index.ts
@@ -42,34 +42,38 @@ router.post(
       throw new RequestValidationError(errors.array())
     }
 
-    const invoiceRepository = getConnection(process.env.NODE_ENV).getRepository(Invoice)
-    const invoice = new Invoice()
-    invoice.clientId = req.body.clientId
-    invoice.issueDate = req.body.issueDate
-    invoice.dueDate = req.body.dueDate
-    invoice.companyId = req.body.companyId
+    try {
+      const invoiceRepository = getConnection(process.env.NODE_ENV).getRepository(Invoice)
+      const invoice = new Invoice()
+      invoice.clientId = req.body.clientId
+      invoice.issueDate = req.body.issueDate
+      invoice.dueDate = req.body.dueDate
+      invoice.companyId = req.body.companyId
 
-    await invoiceRepository.save(invoice)
+      await invoiceRepository.save(invoice)
 
-    const invoiceItemsFromReq = req.body.invoiceItems
-    if (Array.isArray(invoiceItemsFromReq)) {
-      const invoiceItemRepository = getConnection(process.env.NODE_ENV).getRepository(InvoiceItem)
+      const invoiceItemsFromReq = req.body.invoiceItems
+      if (Array.isArray(invoiceItemsFromReq)) {
+        const invoiceItemRepository = getConnection(process.env.NODE_ENV).getRepository(InvoiceItem)
 
-      for (const item of invoiceItemsFromReq) {
-        const newInvoiceItem = new InvoiceItem()
-        newInvoiceItem.name = item.name || ''
-        newInvoiceItem.description = item.description || ''
-        newInvoiceItem.quantity = item.quantity || 0
-        newInvoiceItem.rate = item.rate || 0
-        if (item.taxId) {
-          newInvoiceItem.taxId = item.taxId
+        for (const item of invoiceItemsFromReq) {
+          const newInvoiceItem = new InvoiceItem()
+          newInvoiceItem.name = item.name || ''
+          newInvoiceItem.description = item.description || ''
+          newInvoiceItem.quantity = item.quantity || 0
+          newInvoiceItem.rate = item.rate || 0
+          if (item.taxId) {
+            newInvoiceItem.taxId = item.taxId
+          }
+          newInvoiceItem.invoice = invoice
+          await invoiceItemRepository.save(newInvoiceItem)
         }
-        newInvoiceItem.invoice = invoice
-        await invoiceItemRepository.save(newInvoiceItem)
       }
-    }
 
-    res.status(201).send()
+      res.status(201).send()
+    } catch (err) {
+      throw new Error(err.message)
+    }
   }
 )
 
