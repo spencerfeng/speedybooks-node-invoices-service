df4f0287d54197f41bb8a206d02514f327890ddc
Spencer Feng <spencer.feng@icloud.com>
2020-06-13 16:52:40 +1000

    refactor: create a controller for invoices route

---
+++ src/routes/api/v1/invoices/controller.ts
@@ -0,0 +1,40 @@
+import { getConnection } from 'typeorm'
+import { Request, Response } from 'express'
+
+import { Invoice } from '../../../../entities/Invoice'
+import { InvoiceItem } from '../../../../entities/InvoiceItem'
+
+export const createInvoice = async (req: Request, res: Response) => {
+  try {
+    const invoiceRepository = getConnection(process.env.NODE_ENV).getRepository(Invoice)
+    const invoice = new Invoice()
+    invoice.clientId = req.body.clientId
+    invoice.issueDate = req.body.issueDate
+    invoice.dueDate = req.body.dueDate
+    invoice.companyId = req.body.companyId
+
+    await invoiceRepository.save(invoice)
+
+    const invoiceItemsFromReq = req.body.invoiceItems
+    if (Array.isArray(invoiceItemsFromReq)) {
+      const invoiceItemRepository = getConnection(process.env.NODE_ENV).getRepository(InvoiceItem)
+
+      for (const item of invoiceItemsFromReq) {
+        const newInvoiceItem = new InvoiceItem()
+        newInvoiceItem.name = item.name || ''
+        newInvoiceItem.description = item.description || ''
+        newInvoiceItem.quantity = item.quantity || 0
+        newInvoiceItem.rate = item.rate || 0
+        if (item.taxId) {
+          newInvoiceItem.taxId = item.taxId
+        }
+        newInvoiceItem.invoice = invoice
+        await invoiceItemRepository.save(newInvoiceItem)
+      }
+    }
+
+    res.status(201).send()
+  } catch (err) {
+    throw new Error(err.message)
+  }
+} 
--- src/routes/api/v1/invoices/index.ts
+++ src/routes/api/v1/invoices/index.ts
@@ -1,11 +1,9 @@
 import moment from 'moment'
-import { getConnection } from 'typeorm'
 import express, { Request, Response } from 'express'
-import { validationResult, check } from 'express-validator'
+import { check, validationResult } from 'express-validator'
 
+import { createInvoice } from './controller'
 import { DATE_FORMAT } from '../../../../constants'
-import { Invoice } from '../../../../entities/Invoice'
-import { InvoiceItem } from '../../../../entities/InvoiceItem'
 import { ownCompany } from '../../../../middlewares/ownCompany'
 import { currentUser } from '../../../../middlewares/currentUser'
 import { requireAuth } from '../../../../middlewares/requireAuth'
@@ -29,38 +27,7 @@ router.post(
       throw new RequestValidationError(errors.array())
     }
 
-    try {
-      const invoiceRepository = getConnection(process.env.NODE_ENV).getRepository(Invoice)
-      const invoice = new Invoice()
-      invoice.clientId = req.body.clientId
-      invoice.issueDate = req.body.issueDate
-      invoice.dueDate = req.body.dueDate
-      invoice.companyId = req.body.companyId
-
-      await invoiceRepository.save(invoice)
-
-      const invoiceItemsFromReq = req.body.invoiceItems
-      if (Array.isArray(invoiceItemsFromReq)) {
-        const invoiceItemRepository = getConnection(process.env.NODE_ENV).getRepository(InvoiceItem)
-
-        for (const item of invoiceItemsFromReq) {
-          const newInvoiceItem = new InvoiceItem()
-          newInvoiceItem.name = item.name || ''
-          newInvoiceItem.description = item.description || ''
-          newInvoiceItem.quantity = item.quantity || 0
-          newInvoiceItem.rate = item.rate || 0
-          if (item.taxId) {
-            newInvoiceItem.taxId = item.taxId
-          }
-          newInvoiceItem.invoice = invoice
-          await invoiceItemRepository.save(newInvoiceItem)
-        }
-      }
-
-      res.status(201).send()
-    } catch (err) {
-      throw new Error(err.message)
-    }
+    return await createInvoice(req, res)
   }
 )
 
